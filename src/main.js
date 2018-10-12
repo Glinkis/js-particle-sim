ccad955247588074d4ba2b25c2bbed9a06b1652b
Victor Glindås <glinkis@gmail.com>
2018-10-12 12:39:03 +0200

    Add particles.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+// @ts-check
+const TAU = 2 * Math.PI;
+
 const state = {
+  /** @type {number} */
   timestamp: undefined,
+  /** @type {number} */
   deltaTime: undefined,
+  /** @type {HTMLCanvasElement} */
   canvas: undefined,
+  /** @type {CanvasRenderingContext2D} */
   ctx: undefined,
+  /** @type {Array<{x: number, y: number, radius: number}>} */
   particles: []
 };
 
@@ -11,11 +19,20 @@ function init() {
   state.ctx = state.canvas.getContext("2d");
   document.body.appendChild(state.canvas);
   addEventListener("resize", resize);
+  resize();
   tick();
+
+  setInterval(() => {
+    addParticle({
+      x: Math.random(),
+      y: Math.random(),
+      radius: Math.random() * 20 + 5
+    });
+  }, 250);
 }
 
 function addParticle(params) {
-  const { x = 0.5, y = 0.5, radius = 1 } = params;
+  const { x = 0.5, y = 0.5, radius = 10 } = params || {};
   state.particles.push({ x, y, radius });
 }
 
@@ -29,16 +46,32 @@ function tick(timestamp = 0) {
 }
 
 function update() {
-  const { particles } = state;
+  const { particles, canvas } = state;
   for (const particle of particles) {
-    // Update particle.
+    particle.y += canvas.height / 100000;
+
+    if (particle.y < 0) {
+      particle.y = 0;
+    } else if (particle.y > 1) {
+      particle.y = 1;
+    }
+
+    if (particle.x < 0) {
+      particle.x = 0;
+    } else if (particle.x > 1) {
+      particle.x = 1;
+    }
   }
 }
 
 function draw() {
-  const { particles } = state;
-  for (const particle of particles) {
-    // Draw particle.
+  const { particles, canvas, ctx } = state;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#fff";
+  for (const { x, y, radius } of particles) {
+    ctx.beginPath();
+    ctx.arc(x * canvas.width, y * canvas.height, radius, 0, TAU);
+    ctx.fill();
   }
 }
 
