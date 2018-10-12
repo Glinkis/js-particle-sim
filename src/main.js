ecaf18feb6b4be98f7078133905e363e0d823ef9
Victor Glindås <glinkis@gmail.com>
2018-10-12 12:22:18 +0200

    Add update loops.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,46 @@
 const state = {
-  canvas: document.createElement("canvas")
+  timestamp: undefined,
+  deltaTime: undefined,
+  canvas: undefined,
+  ctx: undefined,
+  particles: []
 };
 
 function init() {
-  addEventListener("resize", resize);
+  state.canvas = document.createElement("canvas");
+  state.ctx = state.canvas.getContext("2d");
   document.body.appendChild(state.canvas);
-  resize();
-  update();
+  addEventListener("resize", resize);
+  tick();
 }
 
-function update() {}
+function addParticle(params) {
+  const { x = 0.5, y = 0.5, radius = 1 } = params;
+  state.particles.push({ x, y, radius });
+}
+
+function tick(timestamp = 0) {
+  state.deltaTime = timestamp - state.timestamp;
+  state.timestamp = timestamp;
+
+  update();
+  draw();
+  requestAnimationFrame(tick);
+}
+
+function update() {
+  const { particles } = state;
+  for (const particle of particles) {
+    // Update particle.
+  }
+}
+
+function draw() {
+  const { particles } = state;
+  for (const particle of particles) {
+    // Draw particle.
+  }
+}
 
 function resize() {
   state.canvas.width = innerWidth * devicePixelRatio;
